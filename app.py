c53c5f9c512755b9d16e481dada95bdbdb5af777
Roozbeh Moradi <roozbehmoradi@Roozbehs-MBP.home>
2021-02-16 12:28:37 +0100

    added calculation of  average deals

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template
+from datetime import *;
+from dateutil.relativedelta import *
+import calendar
 import requests
 import json
 
@@ -12,6 +15,35 @@ headers = {
     "Accept": "application/hal+json",
     "x-api-key": ""
 }
+
+""" Calculate total average deal values for previous year """
+def calculate_average_deals_previuos_year(lime_object):
+    values = []
+
+    for data in lime_object:
+        value = data['value']
+
+        # If e.g "_timestamp" is the data that provides information of the year the  limeobject "deals"
+        timestamp = data['_timestamp']
+        convertedDateOfDeal = datetime.fromisoformat(timestamp).date()
+        dealYear = convertedDateOfDeal.year
+        thisYear = datetime.now().year
+        previuosYear = thisYear - 1
+
+        # If the deals year is previous, add value to list
+        if dealYear == previuosYear:
+            values.append(value)
+
+        # Calculate total average
+    valuesLength = len(values)
+    sumValues = sum(values)
+    averageTotalValuesPreviousYear = sumValues / valuesLength
+
+    # Convert to a whole number
+    convertedValue = int(averageTotalValuesPreviousYear)
+    print('Average: ', convertedValue)
+
+    return convertedValue
 
 
 # Example of function for REST API call to get data from Lime
@@ -48,54 +80,88 @@ def index():
     return render_template('home.html')
 
 
-# Example page
 @app.route('/deals')
 def example():
     # Example of API call to get deals
     base_url = "https://api-test.lime-crm.com/api-test/api/v1/limeobject/deal/"
     params = "?_limit=50"
 
-    # uncomment these 2#######
-    # url = base_url + params
-    # response_deals = get_api_data(headers=headers, url=url)
-
-    heads = [{'name': 'Name'}, {'name': 'avg_deals_pr_month'}, {'name': 'num_deals_pr_mon'},{'name': 'deals_pr_customer'}, {'name': 'status'}]
-
-    print()
-    print(heads)
-    print()
-
-    data = (('Franky hansen', '6 ', '9', '12', 'prospekt'),
-            ('Timmy hansen', '6 ', '9', '12', 'not_interested'),
-            ('Jhon hansen', '0 ', '0', '0', 'Inactive'))
-
-    payload = [{"name": "Tom hansen"}, {'name': 'Franky hansen'}, {'name': 'Timmy hansen'},
-               {"avg": "5"}, {'avg': '6 '}, {'avg': '7 '},
-              {'num': '10'}, {"num": "10"}, {'num': '9'},
-               {'stat': 'prospekt'},{"stat": "customer"},{'stat': 'inactive'},
-               {'won': '12'}, {"won": "11"}, {'won': '13'}]
-
-    a = {'a': 2, 'b': 4, 'c': 6, 'd': 9}
-
-    i = 0
-    for x in a:
-        print(a[x])
-
-    values = a.values()
-
-    total = sum(values)
-
-    avarage = total/4
-    print(avarage)
+    url = base_url + params
+    response_deals = get_api_data(headers=headers, url=url)
 
 
-    # payload = [{"name": "Tom hansen"}, {"name" : "5"}, {"name": "10"}, {"name": "11"}, {"name": "customer"},
-    #            {'name': 'Franky hansen'}, {'name': '6 '}, {'name': '9'}, {'name': '12'}, {'name': 'prospekt'},
-    #            {'name': 'Timmy hansen'}, {'name': '7 '}, {'name': '10'}, {'name': '13'}, {'name': 'inactive'}]
 
+    """ Calculate number of won deals pr month last year  """
+
+    dealsPrMonth = {
+        'january': 0,
+        'february': 0,
+        'march': 0,
+        'april': 0,
+        'may': 0,
+        'june': 0,
+        'july': 0,
+        'august': 0,
+        'september': 0,
+        'october': 0,
+        'november': 0,
+        'december': 0
+    }
+
+    valuesDealsPrMonth = dealsPrMonth.values()
+
+    for key, value in dealsPrMonth.items():
+        print(key, value)
+
+    # Avarage
+    # letters = {'a': 2, 'b': 4, 'c': 6, 'd': 9, 'a': 20}
+    #
+    # for x in letters:
+    #     print(letters[x])
+    #
+    # values = letters.values()
+    #
+    # total = sum(values)
+
+    # average = total / len(letters)
+
+    # print(average)
+
+    # data = (('Franky hansen', '6 ', '9', '12', 'prospekt'),
+    #         ('Timmy hansen', '6 ', '9', '12', 'not_interested'),
+    #         ('Timmy hansen', '6 ', '9', '12', 'not_interested'),
+    #         ('Timmy hansen', '6 ', '9', '12', 'not_interested'),
+    #         ('Timmy hansen', '6 ', '9', '12', 'not_interested'),
+    #         ('Timmy hansen', '6 ', '9', '12', 'not_interested'),
+    #         ('Jhon hansen', '0 ', '0', '0', 'Inactive'))
+
+    payload = [{"name": "Tom hansen"}, {"name": "5"}, {"name": "10"}, {"name": "11"}, {"name": "customer"},
+               {'name': 'Franky hansen'}, {'name': '6 '}, {'name': '9'}, {'name': '12'}, {'name': 'prospekt'},
+               {'name': 'Timmy hansen'}, {'name': '7 '}, {'name': '10'}, {'name': '13'}, {'name': 'inactive'}]
+
+    # payload = [{"name": "Tom hansen"}, {'name': 'Franky hansen'}, {'name': 'Timmy hansen'},
+    #            {"avg": "5"}, {'avg': '6 '}, {'avg': '7 '},
+    #            {'num': '10'}, {"num": "10"}, {'num': '9'},
+    #            {'stat': 'prospekt'}, {"stat": "customer"}, {'stat': 'inactive'},
+    #            {'won': '12'}, {"won": "11"}, {'won': '13'}]
+
+    # count keys occurences for ids
+    sumNumberofName = len([k for d in payload for k in d.keys() if k == 'name'])  # should be 3
+    # print(sumLettersOfA, '-----number of keys -----')
+
+    # second version of count
+    keys_list = []
+    for data in payload:
+        keys_list += data.keys()
+    x = keys_list.count('name')
+    # print(keys_list, '-----number of keys -----')
+
+    table_headers = [{'name': 'Companies'}, {'name': 'Status'}]
+
+    average = calculate_average_deals_previuos_year(response_deals)
 
     if len(data) > 0:
-        return render_template('deals.html', deals=payload, heads=heads)
+        return render_template('deals.html', deals=data, heads=table_headers, average=average)
     else:
         msg = 'No deals found'
         return render_template('deals.html', msg=msg)
@@ -125,3 +191,4 @@ your application
 if __name__ == '__main__':
     app.secret_key = 'somethingsecret'
     app.run(debug=True)
+
